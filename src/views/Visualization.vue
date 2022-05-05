d711398c945ca08abffdbfc6e7e8ec3c850d8d87
Antoine Delahaye <antoine.delahaye@inrae.fr>
2022-05-05 10:25:09 +0200

    Implémentation de la visualisation d'objet

--- src/views/Visualization.vue
+++ src/views/Visualization.vue
@@ -1,7 +1,38 @@
 <script>
+  import {JsonViewer} from 'vue3-json-viewer'
+
+  import 'vue3-json-viewer/dist/index.css'
+
+  export default {
+    data() {
+      return {
+        t: this.i18n.t,
+      }
+    },
+
+    props: {
+      i18n: {
+        type: Object,
+        required: true
+      },
+      yamlStore: {
+        type: Object,
+        required: true
+      }
+    },
+
+    components: {
+      JsonViewer
+    }
+  }
 </script>
 
 <template>
+  <v-main>
+    <v-container fluid>
+      <JsonViewer :value="yamlStore.getYaml"/>
+    </v-container>
+  </v-main>
 </template>
 
 <style scoped>
